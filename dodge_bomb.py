51fd58b9cebe47556c484ff256ff7b07ee806f1c
shisyamotonono <c0b221579f@edu.teu.ac.jp>
2024-06-26 14:08:11 +0900

    移動辞書で書き換え

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,7 +3,6 @@ import sys
 import pygame as pg
 
 
-
 WIDTH, HEIGHT = 1600,900
 DELTA = {
     pg.K_UP: (0, -5),
@@ -30,14 +29,6 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
         for k,v in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
